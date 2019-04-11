d56e90576548b010418b0b1904ef328f5ab0bb6a
jiecheng <email@example.com>
2019-04-11 23:43:16 +0800

    blog首页框架第二稿

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 共有博客项目
 
 ## 资源管理
+
 css 存放样式
 fonts 存放bootstrap字体库
 js 存放页面特效及配置文件
 lib 存放第三方资源
 index 首页
 
+### 第三方资源
+bootstrap@3.3.7
+jquery@3.4.0
+swiper@4.5.0
+
+#### 分工合作
+jc 导航框架
+zh 轮播图
+mz 娃娃
--- js/index.js
+++ js/index.js
@@ -1,22 +1,34 @@
 $(function(){
+	// 导航效果
+	var navTime = 800;//导航切换的时间
 	$('.nav').hover(
 		function(){
 			$(this).animate({
 				left: 0
-			},1000);
+			},navTime);
 			$('.container').animate({
 				left: 200,
 				transform: 'rotate(30deg)'
-			},1000);
+			},navTime);
 		},
 		function(){
 			$(this).animate({
 				left: -190
-			},1000)
+			},navTime)
 			$('.container').animate({
 				left: -10,
 				transform: 'rotate(0deg)'
-			},1000)
+			},navTime)
 		}
 	)
+	
+	// 樱花效果
+	 $('.cherry').Cherry_Blossoms({
+	    is_Cherry:true,//是否生成樱花
+	    image_min:10,//花瓣最小宽度和高度
+	    image_max:50,//花瓣最大宽度和高度
+	    time_min:10000,//花瓣最快下坠时间
+	    time_max:20000,//花瓣最慢下坠时间
+	    interval:500//花瓣生成时间间隔
+	})
 })
